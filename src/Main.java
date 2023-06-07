8d93cbbf558ae55455f470cc11480d1f5e4e42c1
IGNACIO JOSUE ZELADA ARAYA <izeladaa@ucenfotec.ac.cr>
2023-06-07 09:15:53 -0600

    Menu creation

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
 import BTree.BTree;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner input = new Scanner(System.in);
+    private static Scanner intInput = new Scanner(System.in);
     public static void main(String[] args) {
         BTree bArbol = new BTree(3);
-        bArbol.insertarValor(8);
-        bArbol.insertarValor(9);
-        bArbol.insertarValor(10);
-        bArbol.insertarValor(11);
-        bArbol.insertarValor(15);
-        bArbol.insertarValor(20);
-        bArbol.insertarValor(17);
+        boolean seguir = true;
+        while (seguir){
+            System.out.print("Elija una opción"+"\n");
+            System.out.print("1. Insertar un valor"+"\n");
+            System.out.print("2. Mostrar árbol"+"\n");
+            System.out.print("3. Eliminar un valor"+"\n");
+            System.out.print("Presione cualquier otra tecla para salir"+"\n");
 
-        bArbol.MostrarArbol();
+            String opcion = input.nextLine();
+
+            switch (opcion){
+                case "1":
+                    System.out.printf("Escriba el valor: "+"\n");
+                    int v = intInput.nextInt();
+                    bArbol.insertarValor(v);
+                    break;
+                case "2":
+                    System.out.printf("Arbol: "+"\n");
+                    bArbol.MostrarArbol();
+                    System.out.printf("Fin"+"\n");
+                    break;
+                case "3":
+
+                    break;
+                default:
+                    seguir = false;
+                    break;
+            }
+        }
     }
 }
